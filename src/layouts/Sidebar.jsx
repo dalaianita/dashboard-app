31d3cb515aa162f575af4b87b96de97abe6590a5
DalaiAnita <anitadalai9589@gmail.com>
2026-05-25 23:53:17 +0530

    Update sidebar Navlinks using List and keys

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,16 +1,33 @@
 import { NavLink } from "react-router-dom";
 
+
+const navItems = [
+    {id:1 , label: 'Home', path: "/dashboard"},
+    {id:2 , label: 'Profile', path: "profile"},
+    {id:3 , label: 'Settings', path: "settings"},
+    {id:4 , label: 'Users', path: "users"},
+]
+
 function Sidebar() {
   return (
     <aside className="sidebar">
       <nav className="sidebar-links">
-        <NavLink to="/dashboard">Home</NavLink>
+
+        //using list and keys 
+
+        {navItems.map((item)=>(
+            <NavLink key={item.key}
+            to={item.path}> {item.label} </NavLink>))}
+        
+        
+        {     
+        /* <NavLink to="/dashboard">Home</NavLink>
 
         <NavLink to="profile">Profile</NavLink>
 
         <NavLink to="settings">Settings</NavLink>
 
-        <NavLink to="users">Users</NavLink>
+        <NavLink to="users">Users</NavLink> */}
       </nav>
     </aside>
   );
