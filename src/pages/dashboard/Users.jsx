31c70c1ee16ceedabe2c73c274078b745f7dd2f9
DalaiAnita <anitadalai9589@gmail.com>
2026-06-03 23:57:21 +0530

    useMemo and filtered User functionality added

--- src/pages/dashboard/Users.jsx
+++ src/pages/dashboard/Users.jsx
@@ -1,14 +1,24 @@
 import { Link, useSearchParams } from "react-router-dom";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import SearchBox from "../../components/SearchBox";
+
+const users = [
+  { id: 1, name: "Anita" },
+  { id: 2, name: "Sangam" },
+  { id: 3, name: "Banita" },
+];
+
 function Users() {
   const [searchParams, setSearchParams] = useSearchParams();
   const search = searchParams.get("search") || "";
-  const users = [
-    { id: 1, name: "Anita" },
-    { id: 2, name: "Sangam" },
-    { id: 3, name: "Banita" },
-  ];
+
+  const filteredUsers = useMemo(() => {
+    console.log("Filterimng users...");
+
+    return users.filter((user) =>
+      user.name.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [search]);
 
   const handleSearchChange = useCallback(
     (e) => {
@@ -27,6 +37,11 @@ function Users() {
           </Link>
         </div>
       ))}
+
+      {filteredUsers.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+
       {/* <input
         type="text"
         value={search}
