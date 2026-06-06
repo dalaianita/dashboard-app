4248d4e630980c3189542d36f01ab3fd828ecee9
DalaiAnita <anitadalai9589@gmail.com>
2026-06-06 23:55:17 +0530

    console removed

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,6 @@
 import { memo } from "react";
 
 const SearchBox = memo(function SearchBox({ search, onSearchChange }) {
-  console.log("Search box rendered..");
 
   return <input type="text" value={search} onChange={onSearchChange} />;
 });
